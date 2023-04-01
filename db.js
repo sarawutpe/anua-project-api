0c9fa68c3f6ab39db225cb7bd1a1cc45fd8c4406
Sarawut Pe <mr.sarawutpe@gmaiil.com>
2023-04-01 17:57:03 +0700

    make it better

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: '1d1d7546@us-cdbr-east-06.cleardb.net',
+  host: 'us-cdbr-east-06.cleardb.net',
   user: 'be72fd49b94c48',
   password: '7bf324a3',
   database: 'heroku_acab4e5331033a7',
